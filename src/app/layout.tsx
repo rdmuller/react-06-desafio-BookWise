03fe487e949cd05d798ad82eaa0ceb98783fe168
Ronald D. Müller <46840910+rdmuller@users.noreply.github.com>
2023-10-06 12:48:04 -0300

    create database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-900`}>{children}</body>
+      <body className={`${nunito.className} bg-zinc-900`}>
+        {children}
+      </body>
     </html>
   )
 }
