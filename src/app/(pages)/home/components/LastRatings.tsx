74cc058af504027a04628485433d8f86d1f3b6cc
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2023-10-20 05:50:26 -0300

    continuacao

--- src/app/(pages)/home/components/LastRatings.tsx
+++ src/app/(pages)/home/components/LastRatings.tsx
@@ -22,18 +22,20 @@ export const revalidate = 60;
 export default async function LastRatings() {
 	const lastRatings: Rating[] = await api.get("/books/last-ratings", {
 		params: {
-			page_rows: 10
+			page_rows: 2
 		}
 	}).then(res => res.data.lastratings);
 
 	return (
 		<div className="flex flex-col gap-3 w-[38rem]">
 			<span className="text-sm leading-base font-normal">Avaliações mais recentes</span>
-			{lastRatings.map((rating) => {
-				return (
-					<BookCard key={rating.id} author={rating.book.author} bookName={rating.book.name} description={rating.description} created_at={rating.created_at} rate={rating.rate} userName={rating.user.name} bookCoverUrl={rating.book.cover_url} userAvatarUrl={rating.user.avatar_url} />
-				);
-			})}
+			<div className="flex flex-col overflow-auto gap-3">
+				{lastRatings.map((rating) => {
+					return (
+						<BookCard key={rating.id} author={rating.book.author} bookName={rating.book.name} description={rating.description} created_at={rating.created_at} rate={rating.rate} userName={rating.user.name} bookCoverUrl={rating.book.cover_url} userAvatarUrl={rating.user.avatar_url} />
+					);
+				})}
+			</div>
 		</div>
 	);
 }
