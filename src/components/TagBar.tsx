395fd7399aa23f609e74926b6c7f1b696a745c79
Ronald D. Müller <46840910+rdmuller@users.noreply.github.com>
2023-10-27 12:59:57 -0300

    continuacao

--- src/components/TagBar.tsx
+++ src/components/TagBar.tsx
@@ -15,13 +15,24 @@ interface TagBarProps {
 export function TagBar({categories}:TagBarProps) {
 	const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
+	function handleSelectTag(id: string) {
+		setSelectedTags(props => [...props, id]);
+	}
+
+	function handleUnSelectTag(id: string) {
+		const newSelectedTags = selectedTags.filter(tag => tag !== id);
+		setSelectedTags(newSelectedTags);
+	}
+
+	console.log(JSON.stringify(selectedTags));
+
 	return (
 		<div className="flex flex-row flex-wrap gap-3 mt-10">
-			<TagButton>Tudo</TagButton>
+			<TagButton id="all" onSelectTag={handleSelectTag} onUnSelectTag={handleUnSelectTag}>Tudo</TagButton>
 
 			{categories.map(category => {
 				return (
-					<TagButton key={category.id}>{category.name}</TagButton>
+					<TagButton id={category.id} onSelectTag={handleSelectTag} onUnSelectTag={handleUnSelectTag} key={category.id}>{category.name}</TagButton>
 				);
 			})}
 		</div>
--- src/components/TagButton.tsx
+++ src/components/TagButton.tsx
@@ -2,18 +2,30 @@
 
 import { ComponentProps, ElementRef, forwardRef, useState } from "react";
 
-export type TagButtonProps = ComponentProps<"button">
+export type TagButtonProps = ComponentProps<"button"> & {
+	id: string,
+	onSelectTag: (id:string) => void,
+	onUnSelectTag: (id:string) => void
+}
 
 export const TagButton = forwardRef<ElementRef<"button">, TagButtonProps>(
-	({ children, ...props }: TagButtonProps) => {
+	({ id, children, onSelectTag, onUnSelectTag, ...props }: TagButtonProps, ref) => {
 		const [selected, setSelected] = useState(false);
 
 		function handleSelect() {
-			setSelected(!selected);
+			const newStateOfSelect = !selected;
+
+			setSelected(newStateOfSelect);
+
+			if (newStateOfSelect) {
+				onSelectTag(id);
+			} else {
+				onUnSelectTag(id);
+			}
 		}
 
 		return (
-			<button onClick={handleSelect} className={`${selected ? "bg-purple-200 text-gray-100" : "shadow-tag text-purple-100"} px-4 py-1 rounded-full textarea-md leading-base hover:text-gray-100 hover:bg-purple-200 hover:shadow-tag`} {...props}>
+			<button onClick={handleSelect} className={`${selected ? "bg-purple-200 text-gray-100" : "shadow-tag text-purple-100"} px-4 py-1 rounded-full textarea-md leading-base hover:text-gray-100 hover:bg-purple-200 hover:shadow-tag`} {...props} ref={ref}>
 				{children}
 			</button>
 		);
